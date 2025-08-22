ce146215b359598fd00b33e10b3ee3a8a3b23500
theodora-sin <theodrasin@gmail.com>
2025-08-22 12:22:41 +0800

    Update configuation.js

--- configuation.js
+++ configuation.js
@@ -1,4 +1,7 @@
-module.exports={
-    PORT:ProcessingInstruction.env.PORT||5000,
-    ALLOWED_ORIGINS: "#"
+// Simple configuration object
+const Config = {
+  PORT: process.env.PORT || 5000,
+  FERNET_KEY_PATH: process.env.FERNET_KEY_PATH || "fernet.key",
 };
+
+module.exports = { Config };
